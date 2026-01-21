24c1d706b2df517e75177689b009fd959f3c4a69
Abdullahi Abdikarin Abdullahi <apdilaahiapdi143@gmail.com>
2026-01-21 21:33:15 +0300

    submiting exercise 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { Usercard } from './User';
-import Blog from './Blog';
+
+
+import UserCard from './Usercard';
 
 function App() {
+  const firstName = "abdullahi";
+  const lastName = "mohamed";
+  const email = "abdullahi.mohamed@example.com";
+
   return (
     <div>
-      <Blog />
-      
+      <UserCard firstName={firstName} lastName={lastName} email={email} />
+      <UserCard firstName="hamuuda" lastName="mc" email="mc.doe@example.com" />
+      <UserCard firstName="omartood" lastName="tood" email="exercise.doe@example.com" />
+      <UserCard firstName="salah" lastName="adan" email="tood.doe@example.com" />
     </div>
+      
+      
   );
 }
 
---
+++ src/Usercard.jsx
@@ -0,0 +1,9 @@
+const UserCard = (props) => {
+    return (
+      <>
+      <h1> your fisrt name :{props.firstName}</h1>
+       <h1> your last name :{props.lastName}</h1>
+       <h1> your email :{props.email}</h1>
+</>);
+  }
+  export default UserCard;
